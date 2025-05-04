346bac47b490ffbeaa6d7de2c1c1173435c1136f
Callan Barrett <callan@zoocar.org>
2025-05-04 16:25:00 +0800

    Fix dev server

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,18 @@ import { defineConfig, ServerOptions } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   let server: ServerOptions | undefined = undefined;
-  if (command === "serve" && mode === "development") {
+  if (
+    command === "serve" &&
+    mode === "development" &&
+    process.env.DEV_SERVER_IP
+  ) {
     server = {
       host: "0.0.0.0",
       port: 8100
