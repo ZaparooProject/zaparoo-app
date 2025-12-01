3dad72948509080d95bf917231de4c3155793695
Callan Barrett <callan@zoocar.org>
2025-12-01 16:12:29 +0800

    feat: update contributor list and language options
    - Add "Anime0t4ku" to Dutch language credit.
    - Expand contributor list with "disctoad".

--- src/routes/settings.about.tsx
+++ src/routes/settings.about.tsx
@@ -67,7 +67,7 @@ function About() {
             <span>French/Français</span>
           </div>
           <div className="flex flex-row justify-between">
-            <span>Phoenix</span>
+            <span>Phoenix, Anime0t4ku</span>
             <span>Dutch/Nederlands</span>
           </div>
           <div className="flex flex-row justify-between">
@@ -96,27 +96,24 @@ function About() {
           <div className="text-center">
             Jon, <span style={{ color: "#F1C40D" }}>RetroRGB</span>,{" "}
             <span style={{ color: "#F1C40D" }}>Jose BG</span>,{" "}
-            <span style={{ color: "#F1C40D" }}>Mark DeRidder</span>,{" "}
             <span style={{ color: "#F1C40D" }}>Dan Doyle</span>,{" "}
             <span style={{ color: "#F1C40D" }}>Phil Felice</span>,{" "}
             <span style={{ color: "#F1C40D" }}>Glenn</span>,{" "}
             <span style={{ color: "#F1C40D" }}>Alexander Facchini</span>,{" "}
             <span style={{ color: "#F1C40D" }}>Lu&apos;s Retro Source</span>,{" "}
-            <span style={{ color: "#F1C40D" }}>Alexis Conrad</span>,{" "}
             <span style={{ color: "#F1C40D" }}>Tony Shadwick</span>,{" "}
             <span style={{ color: "#F1C40D" }}>Clinton Cronin</span>,{" "}
-            Tuxosaurus, <span style={{ color: "#F1C40D" }}>EntirelyTom</span>,{" "}
-            <span style={{ color: "#F1C40D" }}>the_remora</span>,{" "}
+            Tuxosaurus,{" "}
             <span style={{ color: "#E74C3C" }}>Retrosoft Studios</span>, Casey
             McGinty, <span style={{ color: "#E91E63" }}>Biddle</span>,{" "}
-            <span style={{ color: "#F1C40D" }}>Chris Platts</span>,{" "}
             <span style={{ color: "#F1C40D" }}>
               Gentlemen&apos;s Pixel Club
             </span>
             , <span style={{ color: "#F1C40D" }}>VolJoe</span>,{" "}
             <span style={{ color: "#F1C40D" }}>Shijuro</span>,{" "}
             <span style={{ color: "#F1C40D" }}>Tim Sullivan</span>,{" "}
-            <span style={{ color: "#F1C40D" }}>TheJesusFish</span>
+            <span style={{ color: "#F1C40D" }}>TheJesusFish</span>,{" "}
+            <span style={{ color: "#F1C40D" }}>disctoad</span>
           </div>
 
           <Button
